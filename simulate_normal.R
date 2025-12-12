fb7bf60ff75deae31c4345b2084496af4c0149ec
Joseph Marsh <joe.s.marsh@gmail.com>
2025-12-12 11:36:04 +0000

    simulate normal example

---
+++ simulate_normal.R
@@ -0,0 +1,14 @@
+# Simulate data from a normal distribution and save to CSV
+
+N <- 10000
+muX <- 2.5
+varX <- 1.5
+
+X <- rnorm(N, muX, varX)
+
+mean(X)
+var(X)
+
+cat("Mean of X:", mean(X), ", variance of X:", var(X), "\n")
+
+write.csv(data.frame(X = X), file = "simulated_data.csv", row.names = FALSE)
